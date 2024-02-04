81d8906e157a07a964a803867a270275f2e8d026
Katie <clearykatelyn@gmail.com>
2024-02-04 21:05:00 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+This little game was created purely for fun. Using JS, JSON and HTML/CSS. Hope you enjoy! 
+
+This project was originally inspired by this tutorial: https://www.youtube.com/watch?v=R1S_NhKkvGA
+
+All images are free to use stock images under the Unsplash license: https://unsplash.com/license
+
+Have a nice day! (^0^)/~
+
+kati-e 
+Last Updated: 04/02/2024
---
+++ adventure.js
@@ -0,0 +1,123 @@
+const boardContainer = document.getElementById("adventure-board");
+const scenarioScene = document.getElementById("scene");
+const currentRole = document.getElementById("role");
+const scenarioImage = document.getElementById("scene-image");
+const scenarioText = document.getElementById("scenario-text");
+const optionButtonContainer = document.getElementById("board-options");
+
+let state = { role: null, items: [], status: null };
+
+runScenarios();
+
+async function runScenarios(id) {
+  try {
+    const storyData = await fetchData();
+    const scenarios = storyData.scenarios;
+    showScenario(scenarios[id ? id - 1 : 0]); //success, run the scenario
+  } catch (error) {
+    console.error("Error: ", error);
+    showError(); //failed, hide scenario & show error message
+  }
+}
+
+function pickOption(option) {
+  if (option.requires != null) {
+    let hasItem = false;
+    //check if the required item is in the state
+    state.items.forEach((item) => {
+      if (option.requires.includes(item.name)) {
+        hasItem = true;
+      } else {
+        hasItem = false;
+      }
+    });
+
+    return hasItem;
+  } else {
+    return true;
+  }
+}
+
+function showScenario(scenario) {
+  // Setting inner text/values for elements to display the scenario
+  scenarioScene.innerText = scenario.scene;
+  scenarioImage.style.backgroundImage = `url('${scenario.sceneImage}')`;
+  scenarioText.innerText = scenario.scenarioText;
+  if (state.status !== "dead") {
+    currentRole.innerText = `Role: ${state.role ?? "Traveller"}`;
+  } else {
+    currentRole.innerHTML = `Dead`;
+  }
+  scenario.options.forEach((option) => {
+    let buttonClasses = "";
+
+    if (option.end && state.status === "dead") {
+      buttonClasses = "fail";
+    } else if (option.end && state.status !== "dead") {
+      buttonClasses = "success";
+    }
+
+    if (pickOption(option)) {
+      const button = document.createElement("button");
+      button.innerText = option.optionText;
+      button.classList.add("option");
+      buttonClasses ? button.classList.add(buttonClasses) : null;
+      button.addEventListener("click", () => selectOption(option));
+      optionButtonContainer.appendChild(button);
+    }
+  });
+}
+
+function selectOption(option) {
+  // Set role
+  if (option.newRole) {
+    state.role = option.newRole;
+    console.log(state);
+  }
+  // Set items array
+  if (option.pickupItem) {
+    state.items.push(option.pickupItem);
+  }
+
+  while (optionButtonContainer.firstChild) {
+    optionButtonContainer.removeChild(optionButtonContainer.firstChild);
+  }
+
+  if (option.newRole === "Dead") {
+    state.status = "dead";
+  }
+
+  if (option.end) {
+    state.status = "alive";
+    state.role = null;
+    state.items = [];
+  }
+
+  runScenarios(option.nextScenario);
+}
+
+async function fetchData() {
+  try {
+    const response = await fetch("story.json");
+    if (!response.ok) {
+      throw new Error("It seems there was an error. So sad.");
+    }
+    return response.json();
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+}
+
+function showError() {
+  // Get rid of all the empty elements on the board since the fetch has failed and they will not be filled
+  while (boardContainer.firstChild) {
+    boardContainer.removeChild(boardContainer.firstChild);
+  }
+
+  // Add in an error element
+  const errorMessage = document.createElement("p");
+  errorMessage.innerText = "An error has occured. Please refresh the page.";
+  errorMessage.style.textAlign = "center";
+
+  boardContainer.appendChild(errorMessage);
+}
